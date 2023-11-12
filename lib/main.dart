4bd75aed1cf3eb0e5cb56165b7cb2db6d9646f45
halxian <magistralbert@gmail.com>
2023-11-12 13:53:15 +0300

    task-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,99 +10,144 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: TestScreen(),
-      ),
-    );
-  }
-}
-
-class TestScreen extends StatefulWidget {
-  const TestScreen({super.key});
-
-  @override
-  _TestScreenState createState() => _TestScreenState();
-}
-
-class _TestScreenState extends State<TestScreen>
-    with SingleTickerProviderStateMixin {
-  Offset position = const Offset(0.5, 0.5);
-  Color color = Colors.red;
-
-  late AnimationController _controller;
-
-  late final Animation<double> _animation = CurvedAnimation(
-    parent: _controller,
-    curve: Curves.elasticOut,
-  );
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Positioned(
-          left: position.dx,
-          top: position.dy,
-          child: GestureDetector(
-            onPanUpdate: (details) {
-              setState(() {
-                position = Offset(position.dx + details.delta.dx,
-                    position.dy + details.delta.dy);
-              });
-            },
-            onTap: () {
-              setState(() {
-                color = color == Colors.red ? Colors.blue : Colors.red;
-              });
-            },
-            onLongPress: () {
-              _controller.animateTo(Random().nextDouble());
-            },
-            child: RotationTransition(
-              turns: _animation,
-              child: Container(
-                width: 100.0,
-                height: 100.0,
-                color: color,
+        body: SafeArea(
+          child: CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: [
+              SliverToBoxAdapter(
+                child: Image.asset('assets/img/imgCat.jpeg'),
               ),
-            ),
+              const SliverPadding(
+                padding: EdgeInsets.symmetric(vertical: 6),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 15, 175, 255),
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'О себе',
+                        style: GoogleFonts.oswald(
+                          color: Colors.red,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'Flutter developrt',
+                        style: GoogleFonts.oswald(color: Colors.red),
+                      ),
+                      Text(
+                        'Человек',
+                        style: GoogleFonts.oswald(color: Colors.red),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SliverPadding(
+                padding: EdgeInsets.symmetric(vertical: 6),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 15, 175, 255),
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Увлечения',
+                        style: GoogleFonts.lobster(
+                          color: Colors.yellow,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'Мемы',
+                        style: GoogleFonts.lobster(color: Colors.yellow),
+                      ),
+                      Text(
+                        'Спорт',
+                        style: GoogleFonts.lobster(color: Colors.yellow),
+                      ),
+                      Text(
+                        'Машины',
+                        style: GoogleFonts.lobster(color: Colors.yellow),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SliverPadding(
+                padding: EdgeInsets.symmetric(vertical: 6),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 15, 175, 255),
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Опыт в разработке',
+                        style: GoogleFonts.pressStart2p(
+                          color: Colors.green,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'нету(',
+                        style: GoogleFonts.pressStart2p(
+                          color: Colors.green,
+                        ),
+                      ),
+                      Text(
+                        'чуть чуть знаю java, puthon, c++,',
+                        style: GoogleFonts.pressStart2p(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    height: 83,
+                    child: Column(
+                      children: [
+                        const Text('by'),
+                        Image.asset(
+                          'assets/img/imgSurf.jpg',
+                          scale: 3,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
-        Positioned.fill(
-          child: GestureDetector(
-            onPanUpdate: (details) {
-              setState(() {
-                position = Offset(position.dx + details.delta.dx,
-                    position.dy + details.delta.dy);
-              });
-            },
-            onTap: () {
-              setState(() {
-                color = color == Colors.red ? Colors.blue : Colors.red;
-              });
-            },
-            onLongPress: () {
-              _controller.animateTo(Random().nextDouble());
-            },
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
